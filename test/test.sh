defd20e8c7b9ede9080dc8eb34b185c1b15d0ce7
maxibor <maxime.borry@gmail.com>
2018-09-14 17:45:55 +0200

    removed pandoc from testing

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,4 @@
 
 # computeRatio
 ../bin/computeRatio -c1 ./data/sequences_saureus.out -c2 ./data/sequences_ecoli.out -g1 ecoli.fa -g2 saureus.fa -r1 sequencien.r1.fq -r2 sequencing.r2.fq
-pandoc coproID_result.md -o coproID_result.pdf
+# pandoc coproID_result.md -o coproID_result.pdf
