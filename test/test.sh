9977f880e8e6aa4378f3223da451f9f7e67ebcf8
maxibor <maxime.borry@gmail.com>
2018-09-26 15:25:14 +0200

    cleanup

--- test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# computeRatio
-../bin/computeRatio -c1 ./data/sequences_saureus.out -c2 ./data/sequences_ecoli.out -g1 ecoli.fa -g2 saureus.fa -r1 sequencien.r1.fq -r2 sequencing.r2.fq
-# pandoc coproID_result.md -o coproID_result.pdf
