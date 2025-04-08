75e55ca758204ca13ddda359815d61e809f88928
Meriam van Os <84883985+MeriamOs@users.noreply.github.com>
2025-04-09 08:30:13 +1200

    Add MIT license

--- bin/create_acc2tax.py
+++ bin/create_acc2tax.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Written by Maxime Borry, released under the MIT license
+# See https://opensource.org/license/mit for details
+
 import argparse
 import pysam
 from pathlib import Path
